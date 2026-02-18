2c9d3c502e3d76c92423a0bebddc1b6d08262ad8
DanielPincu <ghepin01@easv365.dk>
2026-02-18 17:51:06 +0100

    Attempt: Scaffold API layer for backend integration

---
+++ src/api/auth.api.ts
@@ -0,0 +1,17 @@
+import { http } from './http';
+import type { ILoginPayload, IRegisterPayload } from '../interfaces/auth.interface'
+
+export async function apiLogin(payload: ILoginPayload) {
+  const res = await http.post('/user/login', payload);
+  return res.data;
+}
+
+export async function apiRegister(payload: IRegisterPayload) {
+  const res = await http.post('/user/register', payload);
+  return res.data;
+}
+
+export async function apiMe() {
+  const res = await http.get('/user/me');
+  return res.data;
+}
